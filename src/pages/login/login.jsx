12803dee73867650003bc8e8e44a1cbea50f923a
antonindev <emailescolar727@gmail.com>
2025-05-02 17:32:01 -0300

    .

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -24,7 +24,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch('http://localhost:4000/login', {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ emailOrUsername, password }),
